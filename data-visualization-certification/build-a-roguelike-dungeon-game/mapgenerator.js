e899247ae0c0b4f7d51ad99e34192b365edb4645
Lyapunov <vaskorichard@gmail.com>
2018-03-21 13:28:02 +0100

    Adding random objects to map + adding a map printer.

--- data-visualization-certification/build-a-roguelike-dungeon-game/mapgenerator.js
+++ data-visualization-certification/build-a-roguelike-dungeon-game/mapgenerator.js
@@ -73,7 +73,78 @@ function generateBasicRoomStructure(sizeY,sizeX,world) {
    return [retval,rooms,sarea];
 }
 
-function generateMap(sizeY,sizeX,world) {
+function getMapCell(map,y,x) {
+   if ( y < 0 || y >= map.length || x < 0 || x >= map[y].length ) {
+      return 0;
+   }
+   return map[y][x];
+}
+
+function bfsMap(map, source, goods) {
+   if ( goods.indexOf(getMapCell(map, source.y,source.x)) == -1 ) {
+      return [];
+   }
+   var retval = [];
+   for ( var y = 0; y < map.length; ++y ) {
+      var row = [];
+      for ( var x = 0; x < map[y].length; ++x ) {
+         row.push(0);
+      }
+      retval.push(row);
+   }
+   var hops = {};
+   var findings = [[source.y,source.x]];
+   hops[findings[0]]=false;
+   var level = 1;
+   retval[source.y][source.x] = level;
+   var dirs = [[0,1],[-1,0],[0,-1],[1,0]];
+   while ( findings.length > 0 ) {
+      ++level;
+      var nfindings = [];
+      for ( var i = 0; i < findings.length; ++i ) {
+         for ( var j = 0; j < dirs.length; ++j ) {
+            var nextp = [(findings[i][0]+dirs[j][1]),(findings[i][1]+dirs[j][0])];
+            if ( goods.indexOf(getMapCell(map,nextp[0],nextp[1])) >= 0
+              && hops[nextp] === undefined ) {
+               nfindings.push(nextp);
+               hops[nextp] = findings[i];
+               retval[nextp[0]][nextp[1]] = level;
+            }
+         }
+      }
+      findings = nfindings;
+   }
+   return [retval, level];
+}
+
+function countMap(map,low,hi) {
+   var retval = 0;
+   for ( var y = 0; y < map.length; ++y ) {
+      for ( var x = 0; x < map[y].length; ++x ) {
+         if ( map[y][x] >= low && map[y][x] < hi ) {
+            ++retval;
+         }
+      }
+   }
+   return retval;
+}
+
+function getNthCell(map,low,hi,n) {
+   var counter = 0;
+   for ( var y = 0; y < map.length; ++y ) {
+      for ( var x = 0; x < map[y].length; ++x ) {
+         if ( map[y][x] >= low && map[y][x] < hi ) {
+            if ( counter == n ) {
+               return [y,x];
+            }
+            ++counter;
+         }
+      }
+   }
+   return false;
+}
+
+function generateMap(sizeY,sizeX,world,ept,spt,createEnemy,createStuff) {
    var retval = [];
    var rooms = [];
    var sarea = 0;
@@ -195,8 +266,129 @@ function generateMap(sizeY,sizeX,world) {
    }
 
    console.log('===>',rooms.length,sarea,sarea/sizeX/sizeY);
+
+   if ( createStuff !== undefined && createEnemy !== undefined ) {
+      var [hmap,hlev] = bfsMap(retval,{y:rooms[0][0],x:rooms[0][1]},[world,5]);
+      var helper = [10,Math.floor(hlev/3),Math.floor(2*hlev/3),hlev-2];
+      var numbers= [];
+      for ( var i = 0; i < 3; ++i ) {
+         numbers.push(countMap(hmap,helper[i],helper[i+1]));
+      }
+      var placedict = {};
+      var enemies = [];
+      var stuffs  = [];
+      for ( var i = 0; i < 3; ++i ) {
+         {
+            var ecounter = 0;
+            while ( ecounter < ept ) {
+               var rnum = getRandomInt(numbers[i]);
+               var pos  = getNthCell(hmap,helper[i],helper[i+1],rnum);
+               if ( !placedict[pos] ) {
+                  placedict[pos] = 1;
+                  var hienemy = i;
+                  var loenemy = Math.max(0,i);
+                  enemies.push( createEnemy( pos, ecounter % 2 ? hienemy : loenemy ) );
+                  ++ecounter;
+               }
+            }
+         }
+         {
+            var scounter = 0;
+            while ( scounter < spt ) {
+               var rnum = getRandomInt(numbers[i]);
+               var pos  = getNthCell(hmap,helper[i],helper[i+1],rnum);
+               if ( !placedict[pos] ) {
+                  placedict[pos] = 1;
+                  stuffs.push( createStuff( pos, scounter < 0.9*spt ) );
+                  ++scounter;
+               }
+            }
+         }
+         
+         if ( i != 1 ) {
+            var scounter = 0;
+            while ( scounter < 2 ) {
+               var rnum = getRandomInt(numbers[i]);
+               var pos  = getNthCell(hmap,helper[i],helper[i+1],rnum);
+               if ( !placedict[pos] ) {
+                  placedict[pos] = 1;
+                  stuffs.push( createStuff( pos, 2+(i>1) ) ); // sword
+                  ++scounter;
+               }
+            }
+         }
+         if ( i != 0 ) {
+            var scounter = 0;
+            while ( scounter < 2 ) {
+               var rnum = getRandomInt(numbers[i]);
+               var pos  = getNthCell(hmap,helper[i],helper[i+1],rnum);
+               if ( !placedict[pos] ) {
+                  placedict[pos] = 1;
+                  stuffs.push( createStuff( pos, 4+(i>1) ) ); // shield
+                  ++scounter;
+               }
+            }
+         }
+      }
+      return [retval,enemies,stuffs];
+   } else {
+      return [retval,undefined,undefined];
+   }
+}
+
+function drawPack(pack) {
+   var [map,enemies,stuffs] = pack;
+   var retval = "";
+   for ( var y = 0; y < map.length; ++y ) {
+      var row = "";
+      for ( var x = 0; x < map[y].length; ++x ) {
+         if ( enemies !== undefined ) {
+            var enemyHere = enemies.filter(([pos,code])=>pos[0]==y&&pos[1]==x);
+            if ( enemyHere.length ) {
+               [pos,code] = enemyHere[0];
+               if ( code == 0 ) {
+                  row += "o";
+               } else if ( code == 1 ) {
+                  row += "O";
+               } else if ( code == 2 ) {
+                  row += "@";
+               }
+               continue;
+            }
+         }
+         if ( stuffs !== undefined ) {
+            var stuffHere = stuffs.filter(([pos,code])=>pos[0]==y&&pos[1]==x);
+            if ( stuffHere.length ) {
+               [pos,code] = stuffHere[0];
+               if ( code == 0 ) {
+                  row += "p";
+               } else if ( code == 1 ) {
+                  row += "P";
+               } else if ( code == 2 ) {
+                  row += ",";
+               } else if ( code == 3 ) {
+                  row += "/";
+               } else if ( code == 4 ) {
+                  row += "v";
+               } else if ( code == 5 ) {
+                  row += "V";
+               }
+               continue;
+            }
+         }
+         if ( map[y][x] == 0 ) {
+            row += "#";
+         } else {
+            row += ".";
+         }
+      }
+      row += "\n";
+      retval += row;
+   }
    return retval;
 }
 
 //generateMap(300,100,1);
-console.log(generateMap(100,25,1));
+//var pack = generateMap(100,20,1);
+var pack = generateMap(100,20,1,30,10,(pos,code)=>[pos,code*1],(pos,code)=>[pos,code*1]);
+console.log(drawPack(pack));
