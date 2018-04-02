3f35ce3a6c011e9f53f790104eb5aeb65a40f7dd
Lyapunov <vaskorichard@gmail.com>
2018-04-02 14:29:46 +0200

    Refactoring.

--- data-visualization-certification/build-a-roguelike-dungeon-game/mapgenerator.js
+++ data-visualization-certification/build-a-roguelike-dungeon-game/mapgenerator.js
@@ -316,7 +316,7 @@ class MapGenerator {
                   var pos = this.getNthCell(hmap,wlow,wup,rnum);
                   if ( pos && !placedict[pos] ) {
                      placedict[pos] = 1;
-                     stuffs.push( createStuff( pos, 2+(i>1)*2 ) ); // sword
+                     stuffs.push( createStuff( pos, 4*world-2+(i>1)*2 ) ); // sword
                      ++scounter;
                   }
                }
@@ -328,7 +328,7 @@ class MapGenerator {
                   var pos  = this.getNthCell(hmap,helper[i],helper[i+1],rnum);
                   if ( pos && !placedict[pos] ) {
                      placedict[pos] = 1;
-                     stuffs.push( createStuff( pos, 3+(i>1)*2 ) ); // shield
+                     stuffs.push( createStuff( pos, 4*world-1+(i>1)*2 ) ); // shield
                      ++scounter;
                   }
                }
@@ -431,16 +431,11 @@ function stuffGenerator(pos, code) {
       return [pos,"p"];
    } else if ( code == 1 ) {
       return [pos,"g"];
-   } else if ( code == 2 ) {
-      return [pos,","];
-   } else if ( code == 3 ) {
-      return [pos,"v"];
-   } else if ( code == 4 ) {
-      return [pos,"/"];
-   } else if ( code == 5 ) {
-      return [pos,"V"];
+   } else if ( code % 2 == 0 ) {
+      return [pos,[",","/",",","/",",","/"][(code-2)/2]];
+   } else {
+      return [pos,["v","V","v","V","v","V"][(code-3)/2]];
    }
-   return [pos,"?"];
 }
 
 //generateMap(300,100,1);
